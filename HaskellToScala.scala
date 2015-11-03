2d690482dadef68fa491544ea068f3fa05db8601
felipeanchieta <felipeanchieta@hotmail.com.br>
2015-11-03 00:34:22 -0200

    Started the main object

---
+++ HaskellToScala.scala
@@ -0,0 +1,34 @@
+object HaskellToScala {
+
+	def showStartText() {
+		println("""|Santo André Haskell Interpreter (c) 2015
+				   |Written by Felipe Anchieta and Rodrigo Martins.
+				   |Check the LICENSE file before doing anything stupid.
+				   |This software comes WITHOUT ANY WARRANTY.""".stripMargin)
+	}
+
+	def showInterpreterLine(str : String) {
+		print(str)
+	}
+
+	def processWords() : Boolean {
+		showInterpreterLine("> ")
+		// TODO: Input
+		val input = /* scanner.nextLine() ? */
+
+		// TODO parser
+		if (startParsing(input) == END) {
+			false
+		} else {
+			true
+		}
+	}
+
+	def main(args: Array[String]) {
+		showStartText()
+
+		if (processWords) {
+			processWords
+		}
+  }
+}
